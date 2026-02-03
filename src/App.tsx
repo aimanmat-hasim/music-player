704dde0349a65454cf911b195ac196aba0f0a5e6
Mohammad Aiman <mohammadaimanmathasim@gmail.com>
2026-02-03 21:57:09 +0800

    Refactor App to centralize playback state and handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Controls from "./control";
 import MusicPlayer from "./musicplayer";
 
